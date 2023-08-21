0db200b357068e84bc368f979fed3aecdec98a33
Juraj Doncevic <juraj.doncevic@fer.hr>
2023-08-21 17:43:52 +0200

    about window completed

--- eTheka/eTheka.App/Windows/AboutWindow.axaml.cs
+++ eTheka/eTheka.App/Windows/AboutWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System;
 
 namespace eTheka.App.Windows;
 public partial class AboutWindow : Window
@@ -6,5 +7,24 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
+    }
+
+
+    public void ClickedLink(string linkString)
+    {
+        OpenUriInBrowser(new Uri(linkString));
+    }
+
+    public void OpenUriInBrowser(Uri? uri)
+    {
+        if (uri is not null)
+        {
+            var strUri = uri.OriginalString;
+            var sInfo = new System.Diagnostics.ProcessStartInfo(strUri)
+            {
+                UseShellExecute = true,
+            };
+            System.Diagnostics.Process.Start(sInfo);
+        }
     }
 }
